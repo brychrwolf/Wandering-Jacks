c3e9da9b0d0d0c1ec52b81ea25f0f0b49534bb8a
Bryan Wolfford <bryan@wolfford.com>
2014-04-12 16:02:05 -1000

    Update Retainer.java Javadoc https://www.pivotaltracker.com/story/show/69237894

--- Wandering Jacks/src/Retainer.java
+++ Wandering Jacks/src/Retainer.java
@@ -2,23 +2,25 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- *
- * @author Initia7_B
+ * A retainer is a special stack of cards for use in the game Wandering Jacks.
+ * <p>
+ * Each of the two players have 4 retainers. The goal in Wandering Jacks is to
+ * retain all 4 Jacks, one each in the player's retainers. The first card is
+ * forbidden from being one of few values, cards may be added to retainers,
+ * retainers may be emptied or individual cards may be removed from anywhere
+ * within the stack as according to the rules of the game.
+ * @author Bryan Wolfford
  * @version %I%, %G%
- *
+ * @see WanderingJacks
+ * @see Player
+ * @see Card
  */
 public class Retainer {
-	/**
-	 *
-	 */
 	private List<Card> retainer;
-	/**
-	 *
-	 */
 	private boolean retainsJack;
 
 	/**
-	 *
+	 * Creates a new empty retainer, which by definition, does not retain a Jack.
 	 */
 	Retainer(){
 		retainer = new ArrayList<Card>();
@@ -26,18 +28,22 @@ public class Retainer {
 	}
 
 	/**
-	 *
-	 * @param target
-	 * @return
+	 * Returns true only if the target card is found within the retainer.
+	 * @param target is the card that is searched for in the retainer.
+	 * @return true only if the target card is found within the retainer, and
+	 * false if otherwise.
 	 */
 	public boolean contains(Card target){
 		return retainer.contains(target);
 	}
 
 	/**
-	 *
-	 * @param targetValue
-	 * @return
+	 * Returns true only if any of the cards in the retainer have a value that
+	 * matches the given target value string.
+	 * @param targetValue the string to search for among all the cards' values
+	 * in the retainer
+	 * @return true only if the target value is found within the retainer, and
+	 * false if otherwise.
 	 */
 	public boolean contains(String targetValue){
 		boolean found = false;
@@ -48,16 +54,17 @@ public class Retainer {
 	}
 
 	/**
-	 *
-	 * @return
+	 * Returns true if this retainer retains a Jack.
+	 * @return true if this retainer retains a Jack, false if otherwise.
 	 */
 	public boolean retainsJack(){
 		return retainsJack;
 	}
 
 	/**
-	 *
-	 * @param card
+	 * Adds a Card to the top of the retainer. Restrictions about which cards
+	 * may be put on which retainers are defined in the rules of the game.
+	 * @param card the Card that is to be put in the retainer.
 	 */
 	public void add(Card card){
 		retainer.add(card);
@@ -66,8 +73,8 @@ public class Retainer {
 	}
 
 	/**
-	 *
-	 * @param card
+	 * Removes the given card from the retainer.
+	 * @param card the card to remove from the retainer.
 	 */
 	public void remove(Card card){
 		retainer.remove(card);
@@ -76,15 +83,15 @@ public class Retainer {
 	}
 
 	/**
-	 *
-	 * @return
+	 * Returns true if the retainer is empty.
+	 * @return true id the retainer is empty, false if otherwise.
 	 */
 	public boolean isEmpty(){
 		return (retainer.size() <= 0 ? true : false);
 	}
 
 	/**
-	 *
+	 * Removes all the cards from the retainer, resulting in a empty retainer.
 	 */
 	public void empty(){
 		retainer = new ArrayList<Card>();
