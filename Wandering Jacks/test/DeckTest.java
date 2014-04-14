de916e268e29403d3172c8d1f8262aae4f66b73d
Bryan Wolfford <bryan@wolfford.com>
2014-04-14 12:33:34 -1000

    First "real" test found bug in Deck.shuffleAllCards() https://www.pivotaltracker.com/story/show/69421042 bug logged: https://www.pivotaltracker.com/story/show/69497564

--- Wandering Jacks/test/DeckTest.java
+++ Wandering Jacks/test/DeckTest.java
@@ -1,4 +1,4 @@
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
@@ -6,8 +6,34 @@ import org.junit.Test;
 public class DeckTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void shuffleTest() {
+		/**
+		 * This test relies on these facts:
+		 * <ul>
+		 * <li>the Deck constructor always makes the
+		 * exact same deck, with the same number of cards, all in the same
+		 * order.</li>
+		 * <li>the constructed Deck size is greater much than 1</li>
+		 * </ul>
+		 */
+		// Store the initial deck in an array
+		Deck seedDeck = new Deck(true);
+		int deckSize = seedDeck.cardsLeft();
+		Card[] initialDeck = new Card[deckSize];
+		for(int i = 0; i < deckSize; i++)
+			initialDeck[i] = seedDeck.dealCard();
+		// Create a new deck, shuffle it, then put that in a new array
+		seedDeck = new Deck(true);
+		Card[] shuffledDeck = new Card[deckSize];
+		seedDeck.shuffleAllCards();
+		for(int i = 0; i < deckSize; i++)
+			shuffledDeck[i] = seedDeck.dealCard();
+		// count the number of differences between the two arrays
+		int numCardsDifferent = 0;
+		for(int i = 0; i < deckSize; i++)
+			if(initialDeck[i] != shuffledDeck[i])
+				numCardsDifferent++;
+		assertTrue(numCardsDifferent / deckSize >= .5);
 	}
 
 }
