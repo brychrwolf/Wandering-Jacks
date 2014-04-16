ca4e63d819dfe5b9fd580c50f88fb64df68ce32d
Bryan Wolfford <bryan@wolfford.com>
2014-04-15 18:12:04 -1000

    Referee to know what a retainer contains https://www.pivotaltracker.com/story/show/69597098

--- test/RetainerTest.java
+++ test/RetainerTest.java
@@ -1,13 +1,24 @@
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
 
 public class RetainerTest {
 
+	/**
+	 * This test ensures that Retainer.contains() returns true if a card with
+	 * the same value and suit is found in the deck
+	 */
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testRetainerContains(){
+		Card aJoker = new Card(0,0);
+		Retainer retainer = new Retainer();
+		assertFalse(retainer.contains(aJoker));
+		retainer.add(aJoker);
+		assertTrue(retainer.contains(aJoker));
 	}
+
+
 
 }
