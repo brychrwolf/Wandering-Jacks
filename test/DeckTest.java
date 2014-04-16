be50b94f5dfa83d21b935cb5ac9ba55c25159d32
Bryan Wolfford <bryan@wolfford.com>
2014-04-15 16:01:18 -1000

    New decks have either 52 (standard) or 54 (Standard+Jokers) cards https://www.pivotaltracker.com/story/show/69593622

--- test/DeckTest.java
+++ test/DeckTest.java
@@ -4,15 +4,41 @@ import org.junit.Test;
 
 
 public class DeckTest {
+	/**
+	 * This test ensures that new decks are constructed with either 52
+	 * (standard) or 54 (Standard+Jokers) cards
+	 */
 	@Test
-	public void shuffleSizeTest(){
-		/**
-		 * This test ensures that shuffling a deck, returns a deck with the same size.
-		 * <p>
-		 * Test is achieved by recording the deck's size, shuffling the deck,
-		 * asserting that the deck size hasn't changed, then popping a card off
-		 * the deck repeating.
-		 */
+	public void testNewDecksHave52or54CorrectCards(){
+		//test standard constructors
+		Deck ds1 = new Deck();
+		Deck ds2 = new Deck(false);
+		assertTrue(ds1.cardsLeft() == 52);
+		assertTrue(ds2.cardsLeft() == 52);
+		//test standard + jokers constructor
+		Deck dj = new Deck(true);
+		assertTrue(dj.cardsLeft() == 54);
+		//ensure all standard cards exists
+		for(int suit = 1; suit <= 4; suit++){
+			for(int value = 1; value <= 13; value++){
+				assertTrue(ds1.contains(new Card(value, suit)));
+				assertTrue(ds2.contains(new Card(value, suit)));
+				assertTrue(dj.contains(new Card(value, suit)));
+			}
+		}
+		//ensure jokers exists
+		assertTrue(dj.contains(new Card(Card.JOKER, Card.JOKER)));
+	}
+
+	/**
+	 * This test ensures that shuffling a deck, returns a deck with the same size.
+	 * <p>
+	 * Test is achieved by recording the deck's size, shuffling the deck,
+	 * asserting that the deck size hasn't changed, then popping a card off
+	 * the deck repeating.
+	 */
+	@Test
+	public void testShuffleSize(){
 		Deck seedDeck = new Deck(true);
 		int sizeBefore;
 		int sizeAfter;
@@ -25,15 +51,15 @@ public class DeckTest {
 		}
 	}
 
+	/**
+	 * This test ensures that shuffling a deck, returns a deck with the same contents.
+	 * <p>
+	 * Test is achieved by recording the deck's size, shuffling the deck,
+	 * asserting that the deck size hasn't changed, then popping a card off
+	 * the deck repeating.
+	 */
 	@Test
-	public void shuffleContentsTest(){
-		/**
-		 * This test ensures that shuffling a deck, returns a deck with the same contents.
-		 * <p>
-		 * Test is achieved by recording the deck's size, shuffling the deck,
-		 * asserting that the deck size hasn't changed, then popping a card off
-		 * the deck repeating.
-		 */
+	public void testShuffleContents(){
 		// get two initial contents
 		Deck initialDeck1 = new Deck(true);
 		Deck initialDeck2 = new Deck(true);
@@ -48,17 +74,17 @@ public class DeckTest {
 			assertTrue(initialDeck2.contains(shuffledDeck.dealCard()));
 	}
 
+	/**
+	 * This test relies on these facts:
+	 * <ul>
+	 * <li>the Deck constructor always makes the
+	 * exact same deck, with the same number of cards, all in the same
+	 * order.</li>
+	 * <li>the constructed Deck size is greater much than 1</li>
+	 * </ul>
+	 */
 	@Test
-	public void shuffleTest() {
-		/**
-		 * This test relies on these facts:
-		 * <ul>
-		 * <li>the Deck constructor always makes the
-		 * exact same deck, with the same number of cards, all in the same
-		 * order.</li>
-		 * <li>the constructed Deck size is greater much than 1</li>
-		 * </ul>
-		 */
+	public void testShuffle() {
 		// Store the initial deck in an array
 		Deck seedDeck = new Deck(true);
 		int deckSize = seedDeck.cardsLeft();
